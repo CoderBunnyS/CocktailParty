3f49cedae1daa95ff529f412e43e79112095f34c
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-05-14 14:51:18 -0700

    deploying

--- src/components/CocktailGallery.js
+++ src/components/CocktailGallery.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CocktailCard from './CocktailCard'
 
 const CocktailGallery = ({cocktailData}) => {
-    //console.log(cocktailData)
+    console.log(cocktailData.drinks[0])
     return (
         <div>
         <CocktailCard {...cocktailData.drinks[0]} />   
