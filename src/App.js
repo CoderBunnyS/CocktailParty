f56b404e3cb1fd8b70e9a1bc394595313d3e04be
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-05-16 18:45:17 -0700

    started search functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import CocktailGallery from "./components/CocktailGallery";
 import '../src/'
-//testing testing
+
 
 function App() {
   const [cocktailData, setCocktailData] = useState([])
   const fetchCocktailData = (searchParam) => {
-    let URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+    let URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini'
 
     fetch(URL)
     .then(res => res.json())
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <CocktailGallery cocktailData = {cocktailData}/>
-      <p>Something should be showing up</p>
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,40 @@
-const Header = () => {
+import { useState } from 'react';
 
+const Header = () => {
+    const [searchedValue, setSearchedValue] = useState('')
+    const [selectedTypeValue, setSelectedTypeValue] = useState('')
+
+    const handleInput = (e) => {
+        e.preventDefault()
+        setSearchedValue(e.target.value)
+    }
+
+    const handleSelectChange = (e) => {
+        e. preventDefault()
+        setSelectedTypeValue(e.target.value)
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        fetchCocktailData(searchedValue, selectedTypeValue)
+    }
     return (
         <div>
-            <h1>What to watch</h1>
+            <h1>What to Drink</h1>
             <div>
                 <input
                     type='text'
-                    placeholder='Title'
+                    placeholder='Drink Name'
                 />
                 <select
                     id="selection"
                     name="selection"
                     placeholder='Type'
                 >
-                    <option value="" hidden >Type</option>
-                    <option value="movie">Movies</option>
-                    <option value="series">Series</option>
-                    <option value="episode">Episode</option>
+                    <option value="" hidden >Type of Drink</option>
+                    <option value="strAlcoholic">Alcoholic</option>
+                    <option value="strAlcoholic">Non-Alcoholic</option>
+                    {/* <option value="episode">Episode</option> */}
                 </select>
                 <button>Search</button>
             </div>
