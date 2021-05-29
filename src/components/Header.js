3e96814790140b66399f2b9e4170e34229341625
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-05-29 13:59:32 -0700

    updated search bar and connected to props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,49 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const Header = ({fetchCocktailData}) => {
-    const [searchedValue, setSearchedValue] = useState('')
-    //const [selectedTypeValue, setSelectedTypeValue] = useState('')
+const Header = ({ fetchCocktailData }) => {
+  const [searchedValue, setSearchedValue] = useState("");
+  //const [selectedTypeValue, setSelectedTypeValue] = useState('')
 
-    const handleInput = (e) => {
-        e.preventDefault()
-        setSearchedValue(e.target.value)
-    }
+  const handleInput = (e) => {
+    e.preventDefault();
+    setSearchedValue(e.target.value);
+  };
 
-    // const handleSelectChange = (e) => {
-        
-    //     e.preventDefault()
-    //     setSelectedTypeValue(e.target.value)
-    // }
+  // const handleSelectChange = (e) => {
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        //fetchCocktailData(searchedValue, selectedTypeValue)
-        fetchCocktailData(searchedValue)
-        console.log(fetchCocktailData)
-        document.getElementById('input').value='';
-        
-    }
-    return (
-        <div className='container-header'>
-            <h1 id="rainbow" className='what-name'>Cocktail Party!</h1>
-            <div>
-            
-                <input
-                    id = 'input'
-                    type='text'
-                    placeholder='Search for a drink by name'
-                    onChange={handleInput}
-                />
-                
-                
-                <button onClick={handleClick}>Search</button>
-            </div>
-        </div>
-    );
+  //     e.preventDefault()
+  //     setSelectedTypeValue(e.target.value)
+  // }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    //fetchCocktailData(searchedValue, selectedTypeValue)
+    fetchCocktailData(searchedValue);
+    console.log(fetchCocktailData);
+    document.getElementById("input").value = "";
+  };
+  return (
+    <div className="container-header">
+      <h1 id="rainbow" className="what-name">
+        Cocktail Party!
+      </h1>
+
+      <div class="searchBox">
+        <input
+          class="searchInput"
+          id = 'input'
+          type="text"
+          name=""
+          placeholder="Search"
+          onChange={handleInput}
+        ></input>
+        <button class="searchButton" onClick={handleClick}>
+          <i class="material-icons">Search</i>
+        </button>
+      </div>
+    </div>
+
+  );
 };
 
-export default Header
+export default Header;
